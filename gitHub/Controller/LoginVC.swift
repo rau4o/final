220a3cde74e1c6de504a660b6b381138ac96c42d
rau4o <rau4o@MacBook-Pro-rau4o.local>
2018-03-12 17:01:22 -0700

    lesson 4

---
+++ gitHub/Controller/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  gitHub
+//
+//  Created by rau4o on 12.03.2018.
+//  Copyright © 2018 rau4o. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
---
+++ gitHub/Utillities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  gitHub
+//
+//  Created by rau4o on 12.03.2018.
+//  Copyright © 2018 rau4o. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_LOGIN = "toLogin"
