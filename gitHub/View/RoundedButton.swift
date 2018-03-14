a48360d649ffafb39abca0afea0aadd24afd8b88
rau4o <rau4o@MacBook-Pro-rau4o.local>
2018-03-14 11:00:33 -0700

    lesson 10.

---
+++ gitHub/View/RoundedButton.swift
@@ -0,0 +1,33 @@
+//
+//  RoundedButton.swift
+//  gitHub
+//
+//  Created by rau4o on 14.03.2018.
+//  Copyright © 2018 rau4o. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+        
+    }
+    
+}
